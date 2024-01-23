3e434e47c7feacf3e19af6dc55290a20df17f44e
M Yang <miloyang9@gmail.com>
2024-01-23 16:03:46 +1100

    updated port

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = 3005;
 
 // Start the Apollo server
 const startServer = async (typeDefs, resolvers) => {
