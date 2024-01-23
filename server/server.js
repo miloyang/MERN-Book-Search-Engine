d8b302095eab584acf84c8db13bc0c8c7f846dcd
M Yang <miloyang9@gmail.com>
2024-01-23 22:03:15 +1100

    commit

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ app.use(express.json());
 
 // If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 // Catch-all route where any route that isnt defined is treated as a 404 error
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
   // start the Apollo server
